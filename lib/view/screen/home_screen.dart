8c4f334b5e0bf942f449de12e14fd859537e9e8b
El-fadaly <45339363+El-fadaly@users.noreply.github.com>
2022-06-30 16:05:46 +0200

    creating  project structure

---
+++ lib/view/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  static const String id = 'HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
